2f236117621e2bf80f9e36806f7bf22279e3b824
amalanki <49692717+amalanki@users.noreply.github.com>
2020-11-30 18:06:49 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
-
-    $servername="localhost";
-    $password="";
-    $username="root";
-    $dbname='testdb';
-
-    $conn = new mysqli($servername, $username, $password, $dbname) or die("unable");
-echo "Greate wrk";
-    
+/* Database connection start */
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "unlocked";
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
 ?>
