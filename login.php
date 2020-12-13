85a9ba1bee32a6e242b5e5df2eb2097d030ebcba
amalanki <49692717+amalanki@users.noreply.github.com>
2020-12-13 22:16:12 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>UNLOCKED</title>
+		
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- Third party plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+		
+		<script src="script.js"></script>
+		
+	
+		</head>
+		<body>
+<?php
+ob_start();
+include('header.php');
+include_once("db_connect.php");
+session_start();
+
+if (isset($_POST['login'])) {
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$password = mysqli_real_escape_string($conn, $_POST['password']);
+	$result = mysqli_query($conn, "SELECT * FROM users WHERE email = '" . $email. "' and pass = '" . md5($password). "'");
+	if ($row = mysqli_fetch_array($result)) {
+		$_SESSION['user_id'] = $row['uid'];
+		$_SESSION['user_name'] = $row['user'];
+		header("Location: portfolio.html");
+	} else {
+		$error_message = "Incorrect Email or Password!!!";
+	}
+}
+$image_url='unlocked.jpg';
+
+?>
+
+<img src="<?php echo $image_url;?>">
+
+
+<title>UNLOCKED : Login</title>
+
+
+<div class="container">
+	<center><h2>Welcome, Please Login to continue.</h2>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 well">
+			<form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="loginform">
+				<fieldset>
+					<legend>Login</legend>
+					<div class="form-group">
+						<label for="name">Email</label>
+						<input type="text" name="email" placeholder="Your Email" required class="form-control" />
+					</div>
+					<div class="form-group">
+						<label for="name">Password</label>
+						<input type="password" name="password" placeholder="Your Password" required class="form-control" />
+					</div>
+					<div class="form-group">
+						<input type="submit" name="login" value="Login" class="btn btn-primary" href="portfolio.html"/>
+					</div>
+				</fieldset>
+			</form>
+			<span class="text-danger"><?php if (isset($error_message)) { echo $error_message; } ?></span>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 text-center">
+		New User? <a href="register.php">Sign Up Here</a>
+		</div>
+	</div>
+<script src="script.js"></script>
+		
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+            <div class="container"><div class="small text-center text-muted">Copyright ©️ 2020 - UNLOCKED</div></div>
+        </footer>
+		
+		
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+
+<?php include('footer.php');?>
